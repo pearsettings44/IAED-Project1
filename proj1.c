6a8c5d1d0377c2650d155b1191b21a72639d8939
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-19 18:55:03 +0000

    Add handle_command function

--- proj1.c
+++ proj1.c
@@ -1,8 +1,51 @@
-#include <stdio.h>
-#include <proj1.h>
+/* José Pereira - ist1103252 */
+#include "proj1.h"
 
+#include <stdio.h>
 
 int main()
 {
+    /* execute program until the user sends the 'q' command */
+    while (handle_command())
+    {
+    }
+
     return 0;
+}
+
+/**
+ * Handles command input.
+ * Returns 1 if the program should continue after running the command.
+ * Otherwise, returns 0.
+ */
+int handle_command()
+{
+    char command = getchar();
+    switch (command)
+    {
+    case 'a':
+        /*handle_add_airport_command(); */
+        return 1;
+    case 'l':
+        /* handle_list_airports_command(); */
+        return 1;
+    case 'v':
+        /* handle_add_flight_command(); */
+        return 1;
+    case 'p':
+        /* handle_list_flight_departure_command(); */
+        return 1;
+    case 'c':
+        /* handle_list_flight_arrival_command(); */
+        return 1;
+    case 't':
+        /* handle_forward_date_command(); */
+        return 1;
+    case 'q':
+        /* stop the program */
+        return 0;
+    default:
+        /* ignore undefined commands */
+        return 1;
+    }
 }
--- proj1.h
+++ proj1.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define IDENTIFICADOR_MAX 3
-#define PAIS_MAX 30
-#define CIDADE_MAX 50
-
-#define AEROPORTO_MAX 4O
+/* proj.c */
+int handle_command();
 
 
 #endif
