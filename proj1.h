59b8d5cb33c70768a9d483f326d588a87ccce6cc
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-25 17:32:02 +0000

    28

--- proj1.h
+++ proj1.h
@@ -108,8 +108,7 @@ void handle_add_flight_command(airport airports[], flight flights[],
                                date system_date);
 void handle_list_flight_departure_command(airport airports[], flight flights[]);
 void handle_list_flight_arrival_command(airport airports[], flight flights[]);
-void handle_forward_date_command(/* airport airports[], flight flights[] */);
-void clear_older_flights(airport airports[], flight flights[]);
+void handle_forward_date_command();
 
 /* airport.c */
 void setup_airports(airport airports[]);
